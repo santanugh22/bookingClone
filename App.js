223d6e7aba22ebdc9ab60dc68fd65d0060a223b8
santanugh22 <11463046+santanugh22@users.noreply.github.com>
2023-10-12 08:54:27 +0530

    INIT

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is going to be my Booking app</Text>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
